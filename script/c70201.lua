55ea179ea1356add2b8481fd8ee0c54c414497f6
5h1n4t4n <dotrongthanhvinh@gmail.com>
2026-08-30 19:02:40 +0700

    Update c70201.lua

--- script/c70201.lua
+++ script/c70201.lua
@@ -6,11 +6,11 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_EXTRA)
-	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetCondition(s.spcon)
 	e1:SetOperation(s.spop)
+	e1:SetValue(SUMMON_TYPE_SPECIAL)
 	c:RegisterEffect(e1)
 	--While you control a "Flower Spirit" Fusion/Synchro/Xyz monster, opponent cannot target
 	--other "Flower Spirit" cards you control with card effects
@@ -22,14 +22,16 @@ function s.initial_effect(c)
 	e2:SetTargetRange(0,1)
 	e2:SetCondition(s.protcon)
 	e2:SetTarget(s.prottg)
+	e2:SetValue(aux.tgoval)
 	c:RegisterEffect(e2)
 	--(Quick Effect): Banish 1 "Flower Spirit" card from GY/banishment; negate the activation
 	--of a card or effect, and if you do, banish that card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetCode(EVENT_CHAINING)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,id)
 	e3:SetCost(s.negcost)
@@ -40,87 +42,88 @@ function s.initial_effect(c)
 	--return it to the hand
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,1))
-	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_REMOVE)
-	e4:SetProperty(EFFECT_FLAG_DELAY)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetCountLimit(1,id+1)
+	e4:SetCountLimit(1,id+100)
 	e4:SetCondition(s.rtcon)
 	e4:SetTarget(s.rttg)
 	e4:SetOperation(s.rtop)
 	c:RegisterEffect(e4)
 end
 
---Special Summon procedure filter/condition/operation
 function s.spfilter(c)
 	return c:IsSetCard(0x702) and c:IsAbleToDeckAsCost()
 end
 function s.spcon(e,c)
-	return Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_GRAVE+LOCATION_REMOVED,0,8,nil)
-end
-function s.spop(e,c)
+	if c==nil then return true end
 	local tp=c:GetControler()
-	if Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,8,nil) then
-		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,8,8,nil)
-		if g:GetCount()>0 then
-			Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-		end
+	return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,8,nil)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,8,8,nil)
+	if #g==8 then
+		Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)
 	end
 end
 
---Protection effect: opponent cannot target other "Flower Spirit" cards you control
 function s.protfilter(c)
-	return c:IsSetCard(0x702) and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ))
+	return c:IsSetCard(0x702) and c:IsType(TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ)
 end
 function s.protcon(e)
 	local tp=e:GetHandlerPlayer()
 	return Duel.IsExistingMatchingCard(s.protfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 function s.prottg(e,c)
-	return c:IsSetCard(0x702) and c~=e:GetHandler() and c:GetControler()==e:GetHandlerPlayer()
+	return c:IsSetCard(0x702) and c~=e:GetHandler() and c:IsControler(e:GetHandlerPlayer())
 end
 
---Quick Effect: banish cost, negate activation of a card or effect, banish it
 function s.gyfilter(c)
 	return c:IsSetCard(0x702) and c:IsAbleToRemoveAsCost()
 end
 function s.negcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
-	Duel.Hint(HINT_CARD,0,id)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
 	local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
-	Duel.ConfirmCards(1-tp,g)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsChainNegatable(ev) end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,nil,0,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	local rc=re:GetHandler()
+	if rc:IsRelateToEffect(re) and rc:IsAbleToRemove() then
+		Duel.SetOperationInfo(0,CATEGORY_REMOVE,eg,1,0,0)
+	end
 end
 function s.negop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) then
 		local rc=re:GetHandler()
-		if rc:IsRelateToEffect(re) and rc:IsAbleToRemoveAsCost() then
+		if rc:IsRelateToEffect(re) then
 			Duel.Remove(rc,POS_FACEUP,REASON_EFFECT)
 		end
 	end
 end
 
---Trigger: if a "Flower Spirit" card(s) you control is banished, return a card on the field to the hand
 function s.rtfilter(c,tp)
-	return c:IsSetCard(0x702) and c:GetPreviousControler()==tp
+	return c:IsSetCard(0x702) and c:IsPreviousControler(tp) and c:IsPreviousLocation(LOCATION_ONFIELD)
 end
 function s.rtcon(e,tp,eg,ep,ev,re,r,rp)
 	return eg:IsExists(s.rtfilter,1,nil,tp)
 end
-function s.rttg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsAbleToHand() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function s.rtop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsAbleToHand() then
+	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,tc)
 	end
 end
