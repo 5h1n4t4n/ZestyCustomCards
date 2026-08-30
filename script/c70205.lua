d65d0d578cf51798f43a177ab7cf0d1003fcbbc1
5h1n4t4n <dotrongthanhvinh@gmail.com>
2026-08-30 20:09:52 +0700

    Update c70205.lua

--- script/c70205.lua
+++ script/c70205.lua
@@ -2,15 +2,21 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
+	c:EnableReviveLimit()
 	--Xyz Summon: 2+ Level 4 monsters
-	aux.AddXyzProcedure(c,aux.TRUE,2,99,4)
-	--Alternative material: 2 Normal Monsters (regardless of Level)
-	aux.AddXyzProcedure(c,Card.IsNormal,2,2,0)
-	--(Quick Effect): Detach 1 material; negate the effect of the first card/effect your
-	--opponent activates in response
-	--NOTE: "the first card/effect they activate in response" is approximated with a
-	--temporary registered effect watching for the opponent's next chain link. Please
-	--test this carefully, timing edge cases may need engine-specific adjustment.
+	Xyz.AddProcedure(c,nil,4,2,99)
+	--Alternative: 2 Normal Monsters
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetCondition(s.altcon)
+	e0:SetTarget(s.alttg)
+	e0:SetOperation(s.altop)
+	e0:SetValue(SUMMON_TYPE_XYZ)
+	c:RegisterEffect(e0)
+	--(Quick Effect): Detach 1 material; negate the effect of the first response from opponent
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_QUICK_O)
@@ -18,59 +24,81 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1,id)
 	e1:SetCost(s.detachcost)
-	e1:SetTarget(s.negsettg)
 	e1:SetOperation(s.negsetop)
 	c:RegisterEffect(e1)
-	--(Quick Effect): When a Spell Card is activated: target that card; attach it to this
-	--card as material
+	--(Quick Effect): When a Spell is activated: target it; attach to this card
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_CHAINING)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,id+1)
+	e2:SetCountLimit(1,id+100)
 	e2:SetCondition(s.atcon)
 	e2:SetTarget(s.attg)
 	e2:SetOperation(s.atop)
 	c:RegisterEffect(e2)
 end
 
-function s.detachcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetOverlayCount()>0 end
-	Duel.RemoveOverlayCard(tp,0,1,1,1,REASON_COST)
+function s.nfilter(c,sc,tp)
+	return c:IsType(TYPE_NORMAL) and c:IsFaceup() and c:IsCanBeXyzMaterial(sc)
 end
-function s.negsettg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
+function s.altcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+		and Duel.IsExistingMatchingCard(s.nfilter,tp,LOCATION_MZONE,0,2,nil,c,tp)
 end
-function s.negsetop(e,tp,eg,ep,ev,re,r,rp)
-	local te=Effect.CreateEffect(e:GetHandler())
-	te:SetType(EFFECT_TYPE_FIELD)
-	te:SetCode(EVENT_CHAINING)
-	te:SetRange(LOCATION_MZONE)
-	te:SetCountLimit(1)
-	te:SetCondition(function(te,tp2,eg2,ep2,ev2,re2,r2,rp2)
-		return re2:GetHandlerPlayer()==1-tp and Duel.IsChainNegatable(ev2)
-	end)
-	te:SetOperation(function(te,tp2,eg2,ep2,ev2,re2,r2,rp2)
-		Duel.NegateActivation(ev2)
-		Duel.Reset(te)
-	end)
-	te:SetReset(RESET_PHASE+PHASE_END,1)
-	Duel.RegisterEffect(te,tp)
+function s.alttg(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectMatchingCard(tp,s.nfilter,tp,LOCATION_MZONE,0,2,2,nil,c,tp)
+	if #g==2 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.altop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	c:SetMaterial(g)
+	Duel.Overlay(c,g)
+	g:DeleteGroup()
 end
 
---When a Spell Card is activated: target it, negate it, attach it to this card as material
-function s.atcon(e,tp,eg,ep,ev,re,r,rp)
-	return re:GetHandler():GetType()&TYPE_SPELL==TYPE_SPELL and Duel.IsChainNegatable(ev)
+function s.detachcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function s.attg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,re:GetHandler(),1,0,0)
+function s.negsetop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetOperation(s.chainop)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.chainop(e,tp,eg,ep,ev,re,r,rp)
+	if ep==1-tp then
+		Duel.NegateEffect(ev)
+		e:Reset()
+	end
+end
+
+function s.atcon(e,tp,eg,ep,ev,re,r,rp)
+	return re:IsActiveType(TYPE_SPELL)
+end
+function s.attg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc==re:GetHandler() end
+	if chk==0 then return re:GetHandler():IsCanBeEffectTarget(e) end
+	Duel.SetTargetCard(re:GetHandler())
 end
 function s.atop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	local tc=re:GetHandler()
-	if Duel.NegateActivation(ev) and tc:IsRelateToEffect(re) and c:IsRelateToEffect(e) and c:IsFaceup() then
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		tc:CancelToGrave()
 		Duel.Overlay(c,tc)
 	end
 end
