009cf713df58a59be2e56c7c3b657e39610def2f
fireb2477-code <fireb2477@gmail.com>
2026-09-17 21:35:16 +0700

    update script/c75142012.lua

--- script/c75142012.lua
+++ script/c75142012.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 
 s.listed_series={0x3b}
-s.listed_names={CARD_REDEYES_B_DRAGON, 93969023} -- Black Metal Dragon (93969023)
+s.listed_names={CARD_REDEYES_B_DRAGON, 93969023, 74677422, 89631139}
 
 function s.initial_effect(c)
     --------------------------------------------------
@@ -12,29 +12,29 @@ function s.initial_effect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
     e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetCountLimit(1,id)
     e1:SetTarget(s.thtg)
     e1:SetOperation(s.thop)
     c:RegisterEffect(e1)
     
-    local e1b=e1:Clone()
-    e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
-    c:RegisterEffect(e1b)
+    local e2=e1:Clone()
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2)
 
     --------------------------------------------------
     -- Effect 2: Tribute to place Continuous S/T & Extra Normal Summon
     --------------------------------------------------
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,1))
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1,id+1)
-    e2:SetCost(s.trcost)
-    e2:SetTarget(s.pltg)
-    e2:SetOperation(s.plop)
-    c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id,1))
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1,{id,1})
+    e3:SetCost(s.plcost)
+    e3:SetTarget(s.pltg)
+    e3:SetOperation(s.plop)
+    c:RegisterEffect(e3)
 end
 
 --------------------------------------------------
@@ -61,15 +61,24 @@ end
 --------------------------------------------------
 -- Effect 2 Logic (Place Continuous S/T & Extra Summon)
 --------------------------------------------------
-function s.trcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
     if chk==0 then return e:GetHandler():IsReleasable() end
     Duel.Release(e:GetHandler(),REASON_COST)
 end
 
+function s.is_listed(c, code)
+    if c:IsCode(code) then return true end
+    if c.listed_names then
+        for _,v in pairs(c.listed_names) do
+            if v==code then return true end
+        end
+    end
+    return false
+end
+
 function s.plfilter(c,tp)
-    -- Đã sửa: Dùng c:ListsCode(...) thay cho aux.IsCodeListed(...)
-    return c:IsType(TYPE_CONTINUOUS) and c:IsType(TYPE_SPELL+TYPE_TRAP) 
-        and c:ListsCode(CARD_REDEYES_B_DRAGON) 
+    return (c:IsType(TYPE_CONTINUOUS) and c:IsType(TYPE_SPELL+TYPE_TRAP))
+        and (s.is_listed(c,74677422) or s.is_listed(c,89631139) or c:ListsCode(CARD_REDEYES_B_DRAGON)) 
         and not c:IsForbidden() and c:CheckUniqueOnField(tp)
 end
 
@@ -83,10 +92,9 @@ end
 function s.plop(e,tp,eg,ep,ev,re,r,rp)
     if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-    local g=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
-    local tc=g:GetFirst()
+    local tc=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
     if tc and Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
-        -- Thêm 1 lần Normal Summon trong lượt
+        -- Additional Normal Summon (Đã sửa lại đúng tên hằng số EDOPro)
         local e1=Effect.CreateEffect(e:GetHandler())
         e1:SetDescription(aux.Stringid(id,2))
         e1:SetType(EFFECT_TYPE_FIELD)
