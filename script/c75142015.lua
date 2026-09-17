69964660966344c323af09999031edd566ef4a06
fireb2477-code <fireb2477@gmail.com>
2026-09-17 21:53:59 +0700

    update script/c75142015.lua

--- script/c75142015.lua
+++ script/c75142015.lua
@@ -4,6 +4,7 @@ function s.initial_effect(c)
 	-- Fusion Material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,s.matfilter)
+	
 	-- Must be Special Summoned with "The Claw of Hermos"
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
@@ -11,7 +12,8 @@ function s.initial_effect(c)
 	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e0:SetValue(s.splimit)
 	c:RegisterEffect(e0)
-	-- Special Summon 2
+	
+	-- Special Summon 2 "Red-Eyes"
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_EQUIP)
@@ -22,27 +24,38 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 end
-s.listed_names={46232525}
+
+s.listed_names={46232525, 74677422} -- Hermos (46232525), Red-Eyes Black Dragon (74677422)
 s.listed_series={0x3b}
+s.material={74677422} -- Khai báo để Scarlet Claw nhận diện là Material
+
 function s.matfilter(c,fc,sumtype,tp)
 	return c:IsRace(RACE_DRAGON,fc,sumtype,tp)
 end
+
 function s.splimit(e,se,sp,st)
-	return se and se:GetHandler():IsCode(46232525)
+	-- Sửa: Cho phép gọi bởi lá 46232525 HOẶC các lá có liên quan đến Hermos
+	return se and (se:GetHandler():IsCode(46232525) or se:GetHandler():ListsCode(46232525))
 end
+
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x3b) and c:IsType(TYPE_MONSTER) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+	return c:IsSetCard(0x3b) and c:IsType(TYPE_MONSTER)
 		and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
+
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,2,nil,e,tp)
-		and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
+	if chk==0 then 
+		return Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_FUSION)>=2
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,2,nil,e,tp)
+			and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 
+	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_EXTRA)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 end
+
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.GetLocationCountFromEx(tp,tp,nil,nil)<2 then return end
+	if Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_FUSION)<2 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,2,2,nil,e,tp)
 	if #g==2 and Duel.SpecialSummon(g,0,tp,tp,true,false,POS_FACEUP)==2 then
@@ -80,6 +93,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 end
+
 function s.eqlimit(e,c)
 	return c==e:GetLabelObject()
 end
