13a2164cf78b501654a047c5bb0a48a8eb07140a
zemtsu058-oss <zemtsu058@gmail.com>
2026-09-18 21:04:49 +0700

    Add files via upload

---
+++ constants.lua
@@ -0,0 +1,14 @@
+-- Custom Archetype
+SET_TTF                           = 0x789
+SET_ATERMIS                       = 0x780
+SET_CAT                           = 0x781
+SET_DESIRE_HERO                   = 0x927
+SET_BUCKLE                        = 0x315
+SET_HYPERDIMENSION                = 0x1291
+SET_CASTLE_OF_DREAMS               = 0x782
+SET_WEZAEMON                       = 0x783
+SET_FLOWER_SPIRIT                = 0x702
+SET_HALOVIAN                       = 0x987
+SET_STAR_RAIL                      = 0x986
+-- Custom counter
+COUNTER_MANA                      = 0x177
