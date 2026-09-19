f9fd6423d1a0070f66755ca778ff40aa6d3681e6
totallynotame123-png <totallynotame123@gmail.com>
2026-09-19 23:20:54 +0700

    Add files via upload

--- script/c90600003.lua
+++ script/c90600003.lua
@@ -1,59 +1,91 @@
 -- Sky Striker Ace - The Fallen Automaton
--- ID: 2772337 (Tuyệt đối không thêm số 0 ở đầu ID trong code Lua)
+-- ID: 2772337
 local s,id=GetID()
 
 --------------------------------------------------------------------------------
--- GLOBAL HOOK: Đánh lừa hệ thống khi check điều kiện Main Monster Zone
+-- GLOBAL HOOK: Call Stack Tracing để bẻ khóa điều kiện Sky Striker
 --------------------------------------------------------------------------------
 if not s.global_check then
     s.global_check=true
 
-    -- Hàm kiểm tra xem player (tp) có đang điều khiển "The Fallen Automaton" ngửa và có hiệu lực hay không
+    -- Danh sách ID các lá Phép Sky Striker yêu cầu Main Monster Zone phải trống
+    local ss_spells = {
+        [1475311] = true,   -- Engage!
+        [99550415] = true,  -- Afterburners!
+        [63166095] = true,  -- Jamming Waves!
+        [51613904] = true,  -- Hornet Drones
+        [6128004] = true,   -- Widow Anchor
+        [25733157] = true,  -- Eagle Booster
+        [90726340] = true,  -- Shark Cannon
+        [31952210] = true,  -- Vector Blast
+        [50920401] = true,  -- Scissors Cross
+        [21976007] = true   -- Linkage!
+    }
+
+    -- Hàm "nghe lén" xem script đang gọi hàm có phải là Phép Sky Striker không
+    local function is_skystriker_caller()
+        for i = 2, 7 do
+            local info = debug.getinfo(i, "S")
+            if not info then break end
+            if info.short_src then
+                local id_str = info.short_src:match("c(%d+)%.lua")
+                if id_str and ss_spells[tonumber(id_str)] then
+                    return true
+                end
+            end
+        end
+        return false
+    end
+
+    -- Hàm kiểm tra xem The Fallen Automaton có đang ngửa trên sân hay không
     local function has_automaton(tp)
         return Duel.IsExistingMatchingCard(function(c)
             return c:IsFaceup() and c:IsCode(id) and not c:IsDisabled()
         end, tp, LOCATION_MZONE, 0, 1, nil)
     end
 
-    -- 1. Hook hàm GetFieldGroupCount (Hàm chính mà Engage, Widow Anchor... hay dùng)
-    _G.SkyStriker_Old_GetFieldGroupCount = Duel.GetFieldGroupCount
+    -- 1. Hook hàm đếm nhóm bài chung
+    local old_GetFieldGroupCount = Duel.GetFieldGroupCount
     Duel.GetFieldGroupCount = function(tp, loc1, loc2)
-        if loc1 == LOCATION_MMZONE and loc2 == 0 and not s.hook_lock then
+        if not s.hook_lock and (loc1 == LOCATION_MZONE or loc1 == LOCATION_MMZONE) and loc2 == 0 then
             s.hook_lock = true
-            local bypass = has_automaton(tp)
+            local bypass = is_skystriker_caller() and has_automaton(tp)
             s.hook_lock = false
-            if bypass then return 0 end -- Giả lập trên sân MMZ không có quái thú
+            if bypass then return 0 end -- Giả lập sân trống
         end
-        return _G.SkyStriker_Old_GetFieldGroupCount(tp, loc1, loc2)
+        return old_GetFieldGroupCount(tp, loc1, loc2)
     end
 
-    -- 2. Hook hàm GetLocationCount (Phòng hờ một số custom core dùng hàm này kiểm tra ô trống)
-    _G.SkyStriker_Old_GetLocationCount = Duel.GetLocationCount
-    Duel.GetLocationCount = function(tp, loc, player, reason, zone)
-        if loc == LOCATION_MMZONE and not s.hook_lock then
-            s.hook_lock = true
-            local bypass = has_automaton(tp)
-            s.hook_lock = false
-            if bypass then return 5 end -- Giả lập có đủ 5 ô trống
-        end
-        return _G.SkyStriker_Old_GetLocationCount(tp, loc, player, reason, zone)
-    end
-
-    -- 3. Hook hàm GetMatchingGroupCount
-    _G.SkyStriker_Old_GetMatchingGroupCount = Duel.GetMatchingGroupCount
+    -- 2. Hook hàm đếm nhóm bài theo điều kiện (Engage thường xài hàm này)
+    local old_GetMatchingGroupCount = Duel.GetMatchingGroupCount
     Duel.GetMatchingGroupCount = function(f, tp, loc1, loc2, ex, ...)
-        if loc1 == LOCATION_MMZONE and loc2 == 0 and not s.hook_lock then
+        if not s.hook_lock and (loc1 == LOCATION_MZONE or loc1 == LOCATION_MMZONE) and loc2 == 0 then
             s.hook_lock = true
-            local bypass = has_automaton(tp)
+            local bypass = is_skystriker_caller() and has_automaton(tp)
             s.hook_lock = false
-            if bypass then return 0 end
+            if bypass then return 0 end -- Giả lập sân trống
         end
-        return _G.SkyStriker_Old_GetMatchingGroupCount(f, tp, loc1, loc2, ex, ...)
+        return old_GetMatchingGroupCount(f, tp, loc1, loc2, ex, ...)
+    end
+
+    -- 3. Hook hàm kiểm tra thẻ bài tồn tại (Widow Anchor, Shark Cannon thường xài)
+    local old_IsExistingMatchingCard = Duel.IsExistingMatchingCard
+    Duel.IsExistingMatchingCard = function(f, tp, loc1, loc2, count, ex, ...)
+        if not s.hook_lock and (loc1 == LOCATION_MZONE or loc1 == LOCATION_MMZONE) and loc2 == 0 then
+            s.hook_lock = true
+            local bypass = is_skystriker_caller() and has_automaton(tp)
+            s.hook_lock = false
+            if bypass then return false end -- Báo cáo "không tìm thấy quái thú nào ở MMZ"
+        end
+        return old_IsExistingMatchingCard(f, tp, loc1, loc2, count, ex, ...)
     end
 end
 
+--------------------------------------------------------------------------------
+-- CÁC HIỆU ỨNG CHÍNH CỦA THE FALLEN AUTOMATON
+--------------------------------------------------------------------------------
 function s.initial_effect(c)
-    -- Điều kiện triệu hồi Link: 2 quái thú "Sky Striker"
+    -- Điều kiện triệu hồi Link
     c:EnableReviveLimit()
     Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x115,0x1115),2,2)
 
@@ -69,9 +101,9 @@ function s.initial_effect(c)
     e1:SetOperation(s.negop)
     c:RegisterEffect(e1)
 
-    -- HIỆU ỨNG 2: Bỏ qua điều kiện Ô Quái Thú Chính (Đã được xử lý triệt để bởi Global Hook ở trên)
+    -- HIỆU ỨNG 2: Bỏ qua điều kiện Ô Quái Thú Chính (Đã được tự động xử lý bởi Call Stack Tracing ở trên)
 
-    -- HIỆU ỨNG 3: (Quick Effect - Twice per turn) Gửi 1 lá "Sky Striker" từ Tay/Sân xuống Mộ -> Vô hiệu hóa & trục xuất úp lá đối thủ kích hoạt
+    -- HIỆU ỨNG 3: (Quick Effect) Gửi 1 lá "Sky Striker" -> Vô hiệu hóa & trục xuất úp
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,1))
     e3:SetCategory(CATEGORY_DISABLE+CATEGORY_REMOVE)
@@ -85,7 +117,7 @@ function s.initial_effect(c)
     e3:SetOperation(s.disop)
     c:RegisterEffect(e3)
 
-    -- HIỆU ỨNG 4: Nếu có bài bị trục xuất bởi hiệu ứng lá này -> Lấy 1 lá "Sky Striker" khác tên từ Mộ/Vùng bị trục xuất lên tay
+    -- HIỆU ỨNG 4: Rút/Lấy lại 1 lá "Sky Striker" khác tên
     local e4=Effect.CreateEffect(c)
     e4:SetDescription(aux.Stringid(id,2))
     e4:SetCategory(CATEGORY_TOHAND)
@@ -100,9 +132,6 @@ function s.initial_effect(c)
     c:RegisterEffect(e4)
 end
 
---------------------------------------------------------------------------------
--- LOGIC HIỆU ỨNG 1
---------------------------------------------------------------------------------
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
 end
@@ -132,9 +161,6 @@ function s.negop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
---------------------------------------------------------------------------------
--- LOGIC HIỆU ỨNG 3
---------------------------------------------------------------------------------
 function s.discon(e,tp,eg,ep,ev,re,r,rp)
     return rp==1-tp and Duel.IsChainNegatable(ev)
 end
@@ -166,9 +192,6 @@ function s.disop(e,tp,eg,ep,ev,re,r,rp)
     end
 end
 
---------------------------------------------------------------------------------
--- LOGIC HIỆU ỨNG 4
---------------------------------------------------------------------------------
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
     return re and re:GetHandler()==e:GetHandler()
 end
