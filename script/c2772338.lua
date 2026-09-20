da1af57c5e25855970c8d363389b3ca836fafffc
totallynotame123-png <totallynotame123@gmail.com>
2026-09-20 10:00:29 +0700

    Add files via upload

--- script/c2772338.lua
+++ script/c2772338.lua
@@ -1,5 +1,8 @@
+-- Sky Striker Ace - Violet
+-- ID: 2772337
 local s,id=GetID()
 function s.initial_effect(c)
+	-- Luôn được xem như "Sky Striker Ace - Roze"
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
 	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
@@ -7,6 +10,7 @@ function s.initial_effect(c)
 	e0:SetValue(37351133)
 	c:RegisterEffect(e0)
 
+	-- HIỆU ỨNG 1: SS từ tay hoặc Mộ khi quái Sky Striker được Summon (Tối đa 2 lần/lượt, khóa triệu hồi suốt ván)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -14,6 +18,7 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
 	e1:SetRange(LOCATION_HAND|LOCATION_GRAVE)
+	e1:SetCountLimit(2,id) -- Giới hạn tối đa 2 lần mỗi lượt
 	e1:SetCondition(s.spcon1)
 	e1:SetTarget(s.sptg1)
 	e1:SetOperation(s.spop1)
@@ -22,6 +27,7 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
 
+	-- HIỆU ỨNG 2: (Quick Effect) Tế lá này -> SS Extra Deck + Vô hiệu hóa + Trục xuất úp mặt
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DISABLE+CATEGORY_REMOVE)
@@ -29,19 +35,20 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E+TIMING_MAIN_END)
-	e3:SetCountLimit(1,id)
+	e3:SetCountLimit(1,{id,1})
 	e3:SetCost(Cost.SelfTribute)
 	e3:SetTarget(s.sptg2)
 	e3:SetOperation(s.spop2)
 	c:RegisterEffect(e3)
 
+	-- HIỆU ỨNG 3: Gửi từ sân xuống Mộ -> Thêm Phép Sky Striker hoặc Rank-Up-Magic
 	local e4=Effect.CreateEffect(c)
 	e4:SetDescription(aux.Stringid(id,4))
 	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
 	e4:SetCode(EVENT_TO_GRAVE)
-	e4:SetCountLimit(1,{id,1})
+	e4:SetCountLimit(1,{id,2})
 	e4:SetCondition(s.thcon)
 	e4:SetTarget(s.thtg)
 	e4:SetOperation(s.thop)
@@ -50,6 +57,9 @@ end
 s.listed_names={id,37351133}
 s.listed_series={SET_SKY_STRIKER,SET_SKY_STRIKER_ACE,SET_RANK_UP_MAGIC}
 
+--------------------------------------------------------------------------------
+-- LOGIC HIỆU ỨNG 1
+--------------------------------------------------------------------------------
 function s.cfilter1(c,tp)
 	return c:IsFaceup() and c:IsControler(tp) and c:IsSetCard(SET_SKY_STRIKER)
 end
@@ -64,11 +74,34 @@ function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) then
-		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- Khóa triệu hồi: Chỉ được triệu hồi quái thú "Sky Striker Ace" trong suốt phần còn lại của ván đấu
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetTargetRange(1,0)
+		e1:SetTarget(s.splimit)
+		Duel.RegisterEffect(e1,tp)
+		
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_CANNOT_SUMMON)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e2:SetTargetRange(1,0)
+		e2:SetTarget(s.splimit)
+		Duel.RegisterEffect(e2,tp)
 	end
 end
 
+function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
+	-- Chỉ cho phép summon các quái thuộc chuỗi "Sky Striker Ace"
+	return not (c:IsSetCard(SET_SKY_STRIKER_ACE) and c:IsType(TYPE_MONSTER))
+end
+
+--------------------------------------------------------------------------------
+-- LOGIC HIỆU ỨNG 2
+--------------------------------------------------------------------------------
 function s.spchkfilter2(c,e,tp)
 	return c:IsSetCard(SET_SKY_STRIKER) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 		and Duel.GetLocationCountFromEx(tp,tp,e:GetHandler(),c)>0
@@ -106,6 +139,9 @@ function s.spop2(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+--------------------------------------------------------------------------------
+-- LOGIC HIỆU ỨNG 3
+--------------------------------------------------------------------------------
 function s.thcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
