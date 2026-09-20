96b9032aed037a7a11d195856666fe2fc5442aea
totallynotame123-png <totallynotame123@gmail.com>
2026-09-20 08:53:51 +0700

    Add files via upload

--- script/c90600001.lua
+++ script/c90600001.lua
@@ -1,76 +1,76 @@
 -- Sky Striker Ace - Homura
--- ID: 02772337
+-- ID: 2772337
 local s,id=GetID()
 
 function s.initial_effect(c)
-    -- Luôn được coi là lá bài "Sky Striker Ace - Raye" (ID: 26082229)
+    -- Luôn luôn được xem như "Sky Striker Ace - Raye" (ID: 39726893)
     local e0=Effect.CreateEffect(c)
     e0:SetType(EFFECT_TYPE_SINGLE)
-    e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-    e0:SetCode(EFFECT_CHANGE_CODE)
-    e0:SetValue(26082229)
+    e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e0:SetCode(EFFECT_ADD_CODE)
+    e0:SetRange(LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE+LOCATION_REMOVED)
+    e0:SetValue(39726893)
     c:RegisterEffect(e0)
 
-    -- HIỆU ỨNG 1: Tự Triệu hồi Đặc biệt từ tay khi không có quái thú hoặc chỉ có quái thú "Sky Striker"
+    -- HIỆU ỨNG 1: Nếu bạn không điều khiển quái thú, hoặc chỉ điều khiển quái thú "Sky Striker" -> Triệu hồi đặc biệt từ tay
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id,0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_HAND)
-    e1:SetCondition(s.spcon1)
-    e1:SetTarget(s.sptg1)
-    e1:SetOperation(s.spop1)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    -- HIỆU ỨNG 2: (Quick Effect) Hiến tế -> Triệu hồi 1 quái thú "Sky Striker" Xyz Rank 4 -> Gắn tối đa 2 Phép "Sky Striker" làm nguyên liệu
+    -- HIỆU ỨNG 2: (Quick Effect) Tế lá này -> Triệu hồi Xyz Rank 4 "Sky Striker", gắn tối đa 2 Phép từ Mộ làm nguyên liệu (nếu có từ 3 Phép trở lên trong Mộ)
     local e2=Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id,1))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_QUICK_O)
     e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
     e2:SetRange(LOCATION_MZONE)
+    e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
     e2:SetCountLimit(1,id)
-    e2:SetCost(s.xyzcost)
-    e2:SetTarget(s.xyztg)
-    e2:SetOperation(s.xyzop)
+    e2:SetCost(s.xyztg_cost)
+    e2:SetTarget(s.xyztg_target)
+    e2:SetOperation(s.xyztg_operation)
     c:RegisterEffect(e2)
 
-    -- HIỆU ỨNG 3: Khi ở Mộ + quái thú "Sky Striker" rời sân bởi hiệu ứng đối thủ -> Tự Triệu hồi Đặc biệt + Lấy 1 Phép "Sky Striker" lên tay
+    -- HIỆU ỨNG 3: Nếu lá này ở Mộ và quái thú "Sky Striker" bạn điều khiển rời sân do hiệu ứng của đối thủ -> Triệu hồi đặc biệt lá này, rồi thêm 1 Phép "Sky Striker" từ Deck lên tay
     local e3=Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id,2))
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOHAND)
     e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
     e3:SetCode(EVENT_LEAVE_FIELD)
     e3:SetRange(LOCATION_GRAVE)
-    e3:SetCountLimit(1,id+100)
-    e3:SetCondition(s.gycon)
-    e3:SetTarget(s.gytg)
-    e3:SetOperation(s.gyop)
+    e3:SetCountLimit(1,id+50)
+    e3:SetCondition(s.spcon2)
+    e3:SetTarget(s.sptg2)
+    e3:SetOperation(s.spop2)
     c:RegisterEffect(e3)
 end
 
 --------------------------------------------------------------------------------
--- LOGIC HIỆU ỨNG 1
+-- LOGIC HIỆU ỨNG 1 (SS từ tay)
 --------------------------------------------------------------------------------
 function s.cfilter(c)
-    return c:IsFaceup() and not (c:IsSetCard(0x115) or c:IsSetCard(0x1115))
+    return c:IsFaceup() and not c:IsSetCard(0x115)
 end
 
-function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
-    local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
-    return #g==0 or not g:IsExists(s.cfilter,1,nil)
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
-function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
 
-function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     if c:IsRelateToEffect(e) then
         Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
@@ -78,43 +78,45 @@ function s.spop1(e,tp,eg,ep,ev,re,r,rp)
 end
 
 --------------------------------------------------------------------------------
--- LOGIC HIỆU ỨNG 2
+-- LOGIC HIỆU ỨNG 2 (Quick Effect: Tế bản thân -> Xyz Summon Rank 4 "Sky Striker")
 --------------------------------------------------------------------------------
-function s.xyzcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.xyzfilter(c,e,tp,mc)
+    return c:IsSetCard(0x115) and c:IsRank(4) and c:IsType(TYPE_XYZ) 
+        and mc:IsCanBeXyzMaterial(c,tp)
+        and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+        and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+end
+
+function s.xyztg_cost(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
-    if chk==0 then return c:IsReleasable() end
+    if chk==0 then return c:IsReleasable() 
+        and Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c) end
     Duel.Release(c,REASON_COST)
 end
 
-function s.xyzfilter(c,e,tp)
-    return (c:IsSetCard(0x115) or c:IsSetCard(0x1115)) and c:IsType(TYPE_XYZ) and c:IsRank(4)
-        and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
-        and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
-function s.matfilter(c)
-    return (c:IsSetCard(0x115) or c:IsSetCard(0x1115)) and c:IsType(TYPE_SPELL)
-end
-
-function s.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+function s.xyztg_target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 
-function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+function s.xyztg_operation(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-    local tc=g:GetFirst()
-    if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
-        local spell_count=Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)
-        if spell_count>=3 then
-            local matg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.matfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,nil)
-            if #matg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
-                Duel.BreakEffect()
-                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-                local sg=aux.SelectUnselectGroup(matg,e,tp,1,2,aux.dncheck,1,tp,HINTMSG_XMATERIAL)
+    local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,e:GetHandler())
+    local sc=g:GetFirst()
+    if sc then
+        Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
+        sc:CompleteProcedure()
+        
+        if Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3 
+            and sc:IsLocation(LOCATION_MZONE) then
+            local mg=Duel.GetMatchingGroup(aux.NecroValleyFilter(function(c)
+                return c:IsSetCard(0x115) and c:IsType(TYPE_SPELL)
+            end),tp,LOCATION_GRAVE,0,nil)
+            if #mg>0 and Duel.SelectYesNo(tp, aux.Stringid(id,3)) then
+                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATTACH)
+                local sg=mg:Select(tp,1,2,nil)
                 if #sg>0 then
-                    Duel.Overlay(tc,sg)
+                    Duel.Overlay(sc,sg)
                 end
             end
         end
@@ -122,44 +124,40 @@ function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 --------------------------------------------------------------------------------
--- LOGIC HIỆU ỨNG 3
+-- LOGIC HIỆU ỨNG 3 (Hồi sinh từ Mộ khi quái Sky Striker rời sân do đối thủ)
 --------------------------------------------------------------------------------
-function s.gyfilter(c,tp)
-    return (c:IsPreviousSetCard(0x115) or c:IsPreviousSetCard(0x1115))
-        and c:IsPreviousLocation(LOCATION_MZONE)
-        and c:IsPreviousControler(tp)
-        and c:IsReason(REASON_EFFECT)
-        and c:GetReasonPlayer()==1-tp
+function s.cfilter2(c,tp)
+    return c:IsPreviousControler(tp) and c:IsSetCard(0x115) and c:IsType(TYPE_MONSTER)
+        and c:IsPreviousLocation(LOCATION_MZONE) and c:IsReason(REASON_EFFECT+REASON_BATTLE)
+        and rp~=tp
 end
 
-function s.gycon(e,tp,eg,ep,ev,re,r,rp)
-    return not Duel.IsDamageStep() and eg:IsExists(s.gyfilter,1,nil,tp)
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+    return not eg:IsContains(e:GetHandler()) and eg:IsExists(s.cfilter2,1,nil,tp)
 end
 
-function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
     local c=e:GetHandler()
     if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
         and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-    Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
-function s.thfilter(c)
-    return (c:IsSetCard(0x115) or c:IsSetCard(0x1115)) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
+function s.thfilter2(c)
+    return c:IsSetCard(0x115) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
 
-function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
-    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-        local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+    if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
+        local g=Duel.GetMatchingGroup(s.thfilter2,tp,LOCATION_DECK,0,nil)
         if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
             Duel.BreakEffect()
             Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
             local sg=g:Select(tp,1,1,nil)
-            if #sg>0 then
-                Duel.SendtoHand(sg,nil,REASON_EFFECT)
-                Duel.ConfirmCards(1-tp,sg)
-            end
+            Duel.SendtoHand(sg,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,sg)
         end
     end
 end
