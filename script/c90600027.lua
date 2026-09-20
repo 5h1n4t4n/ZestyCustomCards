f538860d74dc7f9b4902105a0d71e1a16c0c7f90
totallynotame123-png <totallynotame123@gmail.com>
2026-09-20 22:25:39 +0700

    Add files via upload

--- script/c90600027.lua
+++ script/c90600027.lua
@@ -1,11 +1,9 @@
 -- Sky Striker Mecha - Stabilize
--- ID: 02772337
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Kích hoạt: Chọn 1 quái thú "Sky Striker" bạn điều khiển -> Miễn nhiễm phá hủy + Thêm 1 quái "Sky Striker" từ Mộ/Vùng loại bỏ lên tay, nếu có từ 3 Phép trở lên trong Mộ -> Xáo 1 lá đối thủ điều khiển về Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK+CATEGORY_PROTECT)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -15,40 +13,29 @@ function s.initial_effect(c)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={SET_SKY_STRIKER}
-
---------------------------------------------------------------------------------
--- LOGIC KIỂM TRA ĐIỀU KIỆN KÍCH HOẠT (Main Monster Zone trống)
---------------------------------------------------------------------------------
+function s.mmzfilter(c)
+    return c:GetSequence()<5
+end
 function s.actcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 or not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLocation,LOCATION_MZONE),tp,LOCATION_MZONE,0,1,nil)
+	return Duel.GetMatchingGroupCount(s.mmzfilter,tp,LOCATION_MZONE,0,nil)==0
 end
-
---------------------------------------------------------------------------------
--- TARGET & OPERATION
---------------------------------------------------------------------------------
 function s.tgfilter(c)
-	return c:IsFaceup() and c:IsSetCard(SET_SKY_STRIKER) and c:IsType(TYPE_MONSTER)
+	return c:IsFaceup() and c:IsSetCard(0x115) and c:IsType(TYPE_MONSTER)
 end
-
 function s.thfilter(c)
-	return c:IsSetCard(SET_SKY_STRIKER) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+	return c:IsSetCard(0x115) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
 end
-
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
-	
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_REMOVED)
 end
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		-- Quái thú không thể bị phá hủy bởi chiến đấu hoặc hiệu ứng bài trong lượt này
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
@@ -59,18 +46,13 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 		tc:RegisterEffect(e2)
 	end
-
-	-- Thêm 1 quái thú "Sky Striker" từ Mộ hoặc vùng loại bỏ lên tay
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,1,nil)
 	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
 		Duel.ConfirmCards(1-tp,g)
-		
-		-- Kiểm tra xem trong Mộ có từ 3 Phép trở lên hay không
 		if Duel.GetMatchingGroupCount(Card.IsSpell,tp,LOCATION_GRAVE,0,nil)>=3 
 			and Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,nil) 
 			and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
-			
 			Duel.BreakEffect()
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 			local sg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
