87d61cab796104dd10eb171fc1bb1b5dc02e65a2
totallynotame123-png <totallynotame123@gmail.com>
2026-09-21 15:29:42 +0700

    Add files via upload

--- script/c90600039.lua
+++ script/c90600039.lua
@@ -1,11 +1,11 @@
 -- Sky Striker Special Mobilize - Eclipse Blade
--- ID: 90600039
+-- ID: 2772337
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Hiệu ứng 1: Gửi tối đa 2 lá "Sky Striker" từ Deck/Extra Deck xuống Mộ, rồi thêm/Special Summon 1 quái thú "Sky Striker"
+	-- Hiệu ứng 1: Gửi tối đa 2 lá "Sky Striker" từ Deck xuống Mộ, rồi Special Summon 1 quái thú "Sky Striker" từ Deck bỏ qua điều kiện
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCondition(s.condition)
@@ -40,44 +40,27 @@ function s.tgfilter(c)
 end
 
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x115) and c:IsMonster() and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+	return c:IsSetCard(0x115) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil)
-			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
+		return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,2,nil)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,2,nil)
 	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
-		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 		local tc=sg:GetFirst()
 		if tc then
-			local b1=tc:IsAbleToHand()
-			local b2=tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-			local op=0
-			if b1 and b2 then
-				op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
-			elseif b1 then
-				op=0
-			else
-				op=1
-			end
-			if op==0 then
-				Duel.SendtoHand(tc,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,tc)
-			else
-				-- Đảm bảo truyền chính xác playerid (tp) và vị trí gọi quân cờ
-				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
-			end
+			Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
 		end
 	end
 end
@@ -105,7 +88,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
 		Duel.ConfirmCards(1-tp,g)
 		local has_three = Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3
-		if has_three and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
+		if has_three and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
 			Duel.BreakEffect()
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
 			local sg=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
