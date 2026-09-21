6c4dbe6f4f4267bf6da40fde5175dd09db5d5efe
totallynotame123-png <totallynotame123@gmail.com>
2026-09-21 14:49:25 +0700

    Add files via upload

---
+++ script/c90600039.lua
@@ -0,0 +1,121 @@
+-- Sky Striker Special Mobilize - Eclipse Blade
+-- ID: 90600039
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Hiệu ứng 1: Gửi tối đa 2 lá "Sky Striker" từ Deck/Extra Deck xuống Mộ, rồi thêm/Special Summon 1 quái thú "Sky Striker"
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE+CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+	-- Hiệu ứng 2: Tự trục xuất từ Mộ trong Main Phase để tìm Spell/Trap "Sky Striker", nếu có từ 3 Spell trở lên thì đặt 1 lá trực tiếp lên sân
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,1},EFFECT_COUNT_CODE_DUEL)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+
+s.listed_series={0x115}
+
+-- ==================================================
+-- LOGIC HIỆU ỨNG 1
+-- ==================================================
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_MMZONE,0)==0
+end
+
+function s.tgfilter(c)
+	return c:IsSetCard(0x115) and c:IsAbleToGrave()
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x115) and c:IsMonster() and (c:IsAbleToHand() or c:IsCanBeSpecialSummoned(e,0,tp,true,false))
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK+LOCATION_EXTRA)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK+LOCATION_EXTRA,0,1,2,nil)
+	if #g>0 and Duel.SendtoGrave(g,REASON_EFFECT)>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+		local tc=sg:GetFirst()
+		if tc then
+			local b1=tc:IsAbleToHand()
+			local b2=tc:IsCanBeSpecialSummoned(e,0,tp,true,false)
+			local op=0
+			if b1 and b2 then
+				op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+			elseif b1 then
+				op=0
+			else
+				op=1
+			end
+			if op==0 then
+				Duel.SendtoHand(tc,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,tc)
+			else
+				Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP)
+			end
+		end
+	end
+end
+
+-- ==================================================
+-- LOGIC HIỆU ỨNG 2
+-- ==================================================
+function s.thfilter2(c)
+	return c:IsSetCard(0x115) and c:IsSpellTrap() and c:IsAbleToHand()
+end
+
+function s.setfilter(c,tp)
+	return c:IsSetCard(0x115) and c:IsSpell() and (c:IsSSetable() or (c:IsType(TYPE_FIELD) and c:IsActivateable(tp,true,true)))
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,tp,0)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)>0 then
+		Duel.ConfirmCards(1-tp,g)
+		local has_three = Duel.GetMatchingGroupCount(Card.IsType,tp,LOCATION_GRAVE,0,nil,TYPE_SPELL)>=3
+		if has_three and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil,tp) and Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+			local sg=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil,tp)
+			local tc=sg:GetFirst()
+			if tc then
+				if tc:IsType(TYPE_FIELD) then
+					Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+				else
+					Duel.SSet(tp,tc)
+				end
+			end
+		end
+	end
+end
