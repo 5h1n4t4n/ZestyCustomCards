2428ab204eb3213ee7996e5f480828e905773e2d
totallynotame123-png <totallynotame123@gmail.com>
2026-09-21 13:49:09 +0700

    Add files via upload

--- script/c90600024.lua
+++ script/c90600024.lua
@@ -1,75 +1,162 @@
+-- Sky Striker Airspace - Sector Omega
 local s,id=GetID()
 function s.initial_effect(c)
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	e0:SetCode(EVENT_FREE_CHAIN)
-	e0:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	c:RegisterEffect(e0)
-	
+	-- Kích hoạt Field Spell & Hiệu ứng đào 5 lá
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_FZONE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.efftg)
-	e1:SetOperation(s.effop)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+
+	-- Hiệu ứng: Cho phép kích hoạt Sky Striker Spell kể cả khi có quái thú ở Main Monster Zone (Copy từ nguồn)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_ADJUST)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetOperation(s.adjustop)
+	c:RegisterEffect(e2)
+
+	-- Hiệu ứng: Tăng công thủ (100 ATK/DEF cho mỗi Spell trong Mộ) (Copy từ nguồn)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.atktg)
+	e3:SetValue(s.atkval)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x115} -- Setcode Sky Striker
+
+-- ==================================================
+-- HIỆU ỨNG KÍCH HOẠT: Đào 5 lá trên cùng Bộ bài
+-- ==================================================
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end -- Luôn có thể kích hoạt Field Spell
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
-function s.efftg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local c=e:GetHandler()
-	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) and chkc~=c end
-	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=4
-		and Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,0,1,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,0,1,1,c)
+function s.thfilter(c)
+	return c:IsSetCard(0x115) and c:IsAbleToHand()
 end
 
-function s.xyzfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x115) and c:IsType(TYPE_XYZ)
-end
-
-function s.effop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<4 then return end
-	Duel.ConfirmDecktop(tp,4)
-	local g=Duel.GetDecktopGroup(tp,4)
-	local sg=g:Filter(Card.IsSetCard,nil,0x115)
-	if #sg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
-		local tc=sg:Select(tp,1,1,nil):GetFirst()
-		if tc then
-			g:RemoveCard(tc)
-			local b1=tc:IsAbleToHand()
-			local xyz_g=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_MZONE,0,nil)
-			local b2=#xyz_g>0
-			
-			local op=0
-			if b1 and b2 then
-				op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
-			elseif b1 then
-				op=0
-			elseif b2 then
-				op=1
-			else
-				op=2
-			end
-			
-			if op==0 then
-				Duel.DisableShuffleCheck()
-				Duel.SendtoHand(tc,nil,REASON_EFFECT)
-				Duel.ConfirmCards(1-tp,tc)
-				Duel.ShuffleHand(tp)
-			elseif op==1 then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-				local xyz_c=xyz_g:Select(tp,1,1,nil):GetFirst()
-				if xyz_c then
-					Duel.DisableShuffleCheck()
-					Duel.Overlay(xyz_c,tc)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	-- Kiểm tra nếu Deck có từ 5 lá trở lên và người chơi muốn dùng hiệu ứng
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.ConfirmDecktop(tp,5)
+		local g=Duel.GetDecktopGroup(tp,5)
+		if #g>0 then
+			Duel.DisableShuffleCheck()
+			-- Nếu có lá Sky Striker và người chơi muốn lấy lên tay
+			if g:IsExists(s.thfilter,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+				local sg=g:FilterSelect(tp,s.thfilter,1,1,nil)
+				if #sg>0 then
+					Duel.SendtoHand(sg,nil,REASON_EFFECT)
+					Duel.ConfirmCards(1-tp,sg)
+					Duel.ShuffleHand(tp)
 				end
+			end
+			-- Xáo trộn phần còn lại vào Deck
+			Duel.ShuffleDeck(tp)
+		end
+	end
+end
+
+-- ==================================================
+-- HỆ THỐNG HOOK: Override lỗi Main Monster Zone (Từ file c90600024_2.lua)
+-- ==================================================
+if not s.sky_striker_hooked then
+	s.sky_striker_hooked=true
+	
+	local old_GetFieldGroupCount=Duel.GetFieldGroupCount
+	local old_GetFieldGroup=Duel.GetFieldGroup
+	local old_GetMatchingGroupCount=Duel.GetMatchingGroupCount
+	local old_IsExistingMatchingCard=Duel.IsExistingMatchingCard
+
+	local function is_omega_active(tp)
+		return old_IsExistingMatchingCard(function(c) return c:IsFaceup() and c:IsCode(id) and not c:IsDisabled() end, tp, LOCATION_FZONE, 0, 1, nil)
+	end
+
+	local function is_mmzone(loc)
+		if not loc then return false end
+		local mmz = rawget(_G, "LOCATION_MMZONE")
+		return (mmz and loc == mmz)
+	end
+
+	if old_GetFieldGroupCount then
+		Duel.GetFieldGroupCount=function(p, s_loc, o_loc, ...)
+			if is_mmzone(s_loc) and is_omega_active(p) then
+				return 0
+			end
+			return old_GetFieldGroupCount(p, s_loc, o_loc, ...)
+		end
+	end
+
+	if old_GetFieldGroup then
+		Duel.GetFieldGroup=function(p, s_loc, o_loc, ...)
+			if is_mmzone(s_loc) and is_omega_active(p) then
+				return Group.CreateGroup()
+			end
+			return old_GetFieldGroup(p, s_loc, o_loc, ...)
+		end
+	end
+
+	if old_GetMatchingGroupCount then
+		Duel.GetMatchingGroupCount=function(f, p, s_loc, o_loc, ex, ...)
+			if is_mmzone(s_loc) and is_omega_active(p) then
+				return 0
+			end
+			return old_GetMatchingGroupCount(f, p, s_loc, o_loc, ex, ...)
+		end
+	end
+
+	if old_IsExistingMatchingCard then
+		Duel.IsExistingMatchingCard=function(f, p, s_loc, o_loc, ct, ex, ...)
+			if is_mmzone(s_loc) and is_omega_active(p) then
+				return false
+			end
+			return old_IsExistingMatchingCard(f, p, s_loc, o_loc, ct, ex, ...)
+		end
+	end
+end
+
+s.patched_tables = {}
+function s.patch_cfilter()
+	for k,v in pairs(_G) do
+		if type(k)=="string" and k:sub(1,1)=="c" and type(v)=="table" and not s.patched_tables[v] then
+			if type(v.cfilter)=="function" then
+				local old_cfilter=v.cfilter
+				v.cfilter=function(c,...)
+					if Duel.IsExistingMatchingCard(function(tc) return tc:IsFaceup() and tc:IsCode(id) and not tc:IsDisabled() end,c:GetControler(),LOCATION_FZONE,0,1,nil) then
+						return false
+					end
+					return old_cfilter(c,...)
+				end
+				s.patched_tables[v]=true
 			end
 		end
 	end
-	Duel.ShuffleDeck(tp)
+end
+s.patch_cfilter()
+
+function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
+	s.patch_cfilter()
+end
+
+-- ==================================================
+-- HIỆU ỨNG TĂNG ATK/DEF (Từ file c90600024_2.lua)
+-- ==================================================
+function s.atktg(e,c)
+	return c:IsSetCard(0x115)
+end
+function s.atkval(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_SPELL)*100
 end
